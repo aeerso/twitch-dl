7ad961a78096baaa97d9e803d4d78c6f55f0c68b
Alberto Rositani <albertorositani@MacBook-Air-di-Alberto.local>
2019-12-24 18:26:05 +0100

    Error handling and minor tweaks

--- util.py
+++ util.py
@@ -22,9 +22,13 @@ def getHeaders():
     	'Client-ID': 'kimne78kx3ncx6brgo4mv6wki5h1ko'
 	}
     return headers
+
 #Returns a JSON object for printing and naming the output file
 def getVideoInfo(video_id):
     response = requests.get('https://api.twitch.tv/helix/videos?id=' + video_id, headers=getHeaders())
+    if (response.status_code != 200):
+        return quit(bcolors.FAIL + "Error! Video id or url not found" + bcolors.ENDC)
+    #Creating the object
     return {"username": response.json()['data'][0]['user_name'],
             "title": response.json()['data'][0]['title']}
 
@@ -33,18 +37,21 @@ def getAccessToken(video_id):
     #unique_id can be anything 16 char long. Using this instead of random
     cookies = {'unique_id': '123456789012345'}
     response = requests.get('https://api.twitch.tv/api/vods/' + video_id + '/access_token', headers=getHeaders(), cookies=cookies)
+    if (response.status_code != 200):
+        return quit(bcolors.FAIL + "Error! Can't get an access token for this video" + bcolors.ENDC)
     return {"token": response.json()['token'], "sig": response.json()['sig']}
 
 #Formatting the download url
 def getDownloadUrl(video_id):
+    video_tokens = getAccessToken(video_id)
     download_url = "https://usher.ttvnw.net/vod/" + video_id + ".m3u8" + \
 					"?allow_source=true" + \
 					"&p=5760802" + \
 					"&player_backend=mediaplayer" + \
 					"&playlist_include_framerate=true" + \
 					"&reassignments_supported=true" + \
-					"&sig=" + getAccessToken(video_id)['sig'] + \
-					"&token=" + urllib.parse.quote(getAccessToken(video_id)['token']) + \
+					"&sig=" + video_tokens['sig'] + \
+					"&token=" + urllib.parse.quote(video_tokens['token']) + \
 					"&cdm=wv"
     return download_url
 
